98f23af61649d31d76a879b1c1f59c8a6255c978
lixun <1261142605@qq.com>
2018-03-20 14:12:38 +0800

    修改返回值

--- Arithmetic-OC/main.m
+++ Arithmetic-OC/main.m
@@ -20,9 +20,10 @@ static inline NSMutableArray *kGetArray(){
 
 
 #pragma mark - 桶排序
-void bucketsort(){
-	NSArray *b = @[@5,@2,@3,@1,@8];
+NSArray *bucketsort(NSArray *list){
+
 	NSMutableArray *a = @[].mutableCopy;
+	NSMutableArray *c = [NSMutableArray array];
 	
 	//初始化数组a中所有元素都为0
 	for (NSInteger i = 0; i <= 10; i++) {
@@ -30,7 +31,7 @@ void bucketsort(){
 	}
 	
 	//遍历b中所有元素 当出现数组a中出现b中元素 数组a的这个元素就+1
-	for (NSNumber *num in b) {
+	for (NSNumber *num in list) {
 		
 		NSInteger index = [num integerValue];
 		//index = 5,2,3,1,8
@@ -44,73 +45,88 @@ void bucketsort(){
 	}
 	
 	//遍历数组a
-	for (NSInteger i = 10; i < a.count; i --) {
-		//找到数组a中被增加的元素
+	
+	//从大到小
+//	for (NSInteger i = 10; i < a.count; i --) {
+//		//找到数组a中被增加的元素
+//		if ([a[i] integerValue] > 0) {
+//			[c addObject:@(i)];
+//		}
+//	}
+	
+	//从小到大
+	for (NSInteger i = 0; i <= 10; i++) {
 		if ([a[i] integerValue] > 0) {
-			NSLog(@"%ld",i);
+			[c addObject:@(i)];
 		}
+	}
+	
+	if (c.count > 0) {
+		return c;
+	} else {
+		return nil;
 	}
 }
 
+
+
 #pragma mark - 冒泡排序
-void bubblesort(){
+NSArray *bubblesort(NSMutableArray *list){
 	NSNumber *t;
-	NSMutableArray *b = kGetArray();
-	for (NSInteger i = 0; i < b.count - 1; i++) { //n个数排序,只用进行n-1趟
-		for (NSInteger j = 0; j < b.count - 1; j++) { //从第1位开始比较直到最后一位尚未归为的数 因为只进行n-1趟排序,所以最后一位则是n-1。
-			if (b[j] < b[j + 1]) {
-				t = b[j];
-				b[j] = b[j+ 1];
-				b[j + 1] = t;
+	for (NSInteger i = 0; i < list.count - 1; i++) { //n个数排序,只用进行n-1趟
+		for (NSInteger j = 0; j < list.count - 1; j++) { //从第1位开始比较直到最后一位尚未归为的数 因为只进行n-1趟排序,所以最后一位则是n-1。
+			if (list[j] > list[j + 1]) {
+				t = list[j];
+				list[j] = list[j+ 1];
+				list[j + 1] = t;
 			}
 		}
 	}
-	NSLog(@"%@",b);
+	return list;
 }
 
 
-void quicksort(NSMutableArray *array, NSInteger left, NSInteger right){
+NSArray *quicksort(NSMutableArray *array, NSInteger left, NSInteger right){
 	
 	if (left > right) {
-		return;
+		return nil;
 	}
 
 	//temp 基准数
-	NSInteger i, j, t, temp;
+	NSInteger temp = [array[left] integerValue];
+	NSInteger t = 0;
 	
-	temp = [array[left] integerValue];
-	i = left;
-	j = right;
-	
-	while (i != j) {
+	while (left != right) {
 		//从右向左找
-		while ([array[j] integerValue] > temp && i < j) {
-			j --;
+		while ([array[right] integerValue] > temp && left < right) {
+			right --;
 		}
 		
 		//从左向右找
-		while ([array[i] integerValue] < temp && i < j) {
-			i ++;
+		while ([array[left] integerValue] < temp && left < right) {
+			left ++;
 		}
 		
-		//未相遇  如果 i==j代表找到基准数的位置
-		if (i < j) {
+		//未相遇
+		if (left < right) {
 			//交换位置
-			t = [array[i] integerValue];
-			array[i] = array[j];
-			array[j] = @(t);
+			t = [array[left] integerValue];
+			array[left] = array[right];
+			array[right] = @(t);
 		}
 	}
 	
-	//i == j 确定基准数位置
-	array[left] = array[i];
-	array[i] = @(temp);
+	//left == right 确定基准数位置
+	array[left] = @(temp);
 	
 	//递归
 	//处理基准数左边数据
-	quicksort(array, left, i - 1);
+	quicksort(array, left, left - 1);
 	//处理基准数右边数据
-	quicksort(array, i + 1, right);
+	quicksort(array, left + 1, right);
+	
+	return array;
+	
 }
 
 
@@ -118,7 +134,11 @@ void quicksort(NSMutableArray *array, NSInteger left, NSInteger right){
 int main(int argc, char * argv[]) {
 
 	@autoreleasepool {
-		quicksort(kGetArray(), 0, 9);
+//		quicksort(kGetArray(), 0, 9);
+		NSArray *b = @[@5,@2,@3,@1,@8,@6];
+		NSLog(@"%@",bubblesort(b.mutableCopy));
+		
+//		NSLog(@"%@",quicksort([b mutableCopy], 0, 4));
 		
 	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
 	}
